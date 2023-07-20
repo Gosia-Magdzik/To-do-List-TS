e1ff9b71c40c91d2ffb12da452db5e17d822cf09
Gosia Magdzik <melassa26@gmail.com>
2023-07-20 19:40:40 +0200

    useState hook in TS

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Header, Wrapper } from './AppStyle';
 import { Input }  from "./components/Input"
 
 const App: React.FC = () => {
+  const [todo, setTodo] = useState<string>("");
+
   return (
     <Wrapper>
       <Header>TASKIFY</Header>
