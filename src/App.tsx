ed19fba4b4ee78fecda53d7b37ae08642321770b
Gosia Magdzik <melassa26@gmail.com>
2023-07-21 21:46:31 +0200

    model & next useState has been created

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import React, { useState } from 'react';
 import { Header, Wrapper } from './AppStyle';
 import { Input }  from "./components/Input"
+import { Todo } from './model';
 
 const App: React.FC = () => {
   const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([]); //array of todos
+
+const handleAdd = () => {};
 
   return (
     <Wrapper>
