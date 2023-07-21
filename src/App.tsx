c84b26bab09dfe929eb95fd6934a11bc2337044b
Gosia Magdzik <melassa26@gmail.com>
2023-07-21 20:25:53 +0200

    input's prop seeting

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ const App: React.FC = () => {
   return (
     <Wrapper>
       <Header>TASKIFY</Header>
-      <Input/>
+      <Input todo={todo} setTodo={setTodo} />
     </Wrapper>
   );
 }
--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import { Form, InputField, Button } from "./styled"
 
-export const Input = () => {
+interface Props {
+    todo: string;
+    setTodo: React.Dispatch<React.SetStateAction<string>>;
+}
+
+export const Input = ({todo, setTodo} : Props) => {
     return (
         <Form>
             <InputField
                 type='input'
+                value={todo}
+                onChange={
+                    (e)=>setTodo(e.target.value) //e. event obiekt zdarzenia
+                }
                 placeholder='Enter a task'
             />
             <Button
